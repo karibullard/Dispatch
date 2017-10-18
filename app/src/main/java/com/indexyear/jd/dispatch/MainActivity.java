0970692b866f573135f3cb136d0eb13ff9d1eb1c
Kari Bullard <kari.e.bullard@gmail.com>
2017-10-17 23:11:19 -0700

    Created basic data models, began setup for Firebase messaging. Added app main screen and menu.

---
+++ app/src/main/java/com/indexyear/jd/dispatch/MainActivity.java
@@ -0,0 +1,64 @@
+package com.indexyear.jd.dispatch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * Created by karibullard on 10/17/17.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        // [START handle_data_extras]
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
+        // [END handle_data_extras]
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //respond to menu item selection
+        switch (item.getItemId()) {
+//            case R.id.about:
+//                startActivity(new Intent(this, About.class));
+//                return true;
+//            case R.id.help:
+//                startActivity(new Intent(this, Help.class));
+//                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/indexyear/jd/dispatch/models/MCT.java
@@ -0,0 +1,16 @@
+package com.indexyear.jd.dispatch.models;
+
+import java.util.List;
+
+/**
+ * Created by karibullard on 10/17/17.
+ */
+
+class MCT {
+    private String teamName;
+    private List<Employee> teamMembers;
+
+    public MCT(String teamName){
+        this.teamName = teamName;
+    }
+}
